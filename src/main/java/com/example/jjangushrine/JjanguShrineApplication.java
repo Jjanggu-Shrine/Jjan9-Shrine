281c670e2958b6e3e46798a7b4dd1a340d885455
wldnr1208 <wldnr1208@naver.com>
2025-01-31 22:42:20 +0900

    first init

---
+++ src/main/java/com/example/jjangushrine/JjanguShrineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjangushrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjanguShrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjanguShrineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jjangushrine/JjanguShrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjangushrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjanguShrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
