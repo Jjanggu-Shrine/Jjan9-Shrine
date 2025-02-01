87411951e73dbc1bc90e60a9a6423dedc8bb7c62
duol9 <lhyy0195@gmail.com>
2025-02-02 02:22:46 +0900

    fix: SellerRepository Inteface로 수정
    - 커밋 및 푸시 시점에 Seller 담당자가 merge가 되지 않는 이슈로 직접 수정

--- src/main/java/com/example/jjangushrine/domain/seller/repository/SellerRepository.java
+++ src/main/java/com/example/jjangushrine/domain/seller/repository/SellerRepository.java
@@ -1,4 +1,8 @@
 package com.example.jjangushrine.domain.seller.repository;
 
-public class SellerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.jjangushrine.domain.seller.entity.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
 }
