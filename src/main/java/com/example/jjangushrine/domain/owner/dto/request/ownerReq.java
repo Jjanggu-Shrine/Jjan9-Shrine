631079841a1d379db3c2e96fe68b86d132e762c8
goo3oo <goo3oo0918@gmail.com>
2025-02-01 16:40:38 +0900

    refactor: 유저 롤, 도메인명 변경 Owner -> Seller

--- src/main/java/com/example/jjangushrine/domain/owner/dto/request/ownerReq.java
+++
@@ -1,4 +0,0 @@
-package com.example.jjangushrine.domain.owner.dto.request;
-
-public class ownerReq {
-}
---
+++ src/main/java/com/example/jjangushrine/domain/seller/dto/response/SellerRes.java
@@ -0,0 +1,4 @@
+package com.example.jjangushrine.domain.seller.dto.response;
+
+public class SellerRes {
+}
