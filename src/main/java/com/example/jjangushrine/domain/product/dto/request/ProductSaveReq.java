2739240e758bb5f5498d545437df231b75cbebd3
duol9 <lhyy0195@gmail.com>
2025-02-02 02:49:04 +0900

    feat: ProductSaveReq Valid 적용
    - 모든 필드 @NootBlank 적용
    - name 최대길이 20자로 설정

--- src/main/java/com/example/jjangushrine/domain/product/dto/request/ProductSaveReq.java
+++ src/main/java/com/example/jjangushrine/domain/product/dto/request/ProductSaveReq.java
@@ -1,12 +1,29 @@
 package com.example.jjangushrine.domain.product.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ProductSaveReq(
+	@NotBlank
 	Long storeId,
+
+	@NotBlank
+	@Size(max = 20)
 	String name,
+
+	@NotBlank
 	Integer amount,
+
+	@NotBlank
 	String description,
+
+	@NotBlank
 	String image,
+
+	@NotBlank
 	Short stock,
+
+	@NotBlank
 	String category
 ) {
 }
