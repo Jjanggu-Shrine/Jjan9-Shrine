2fc9f3891b5059928e30ea43711fde4b31b89acc
duol9 <lhyy0195@gmail.com>
2025-02-02 02:49:25 +0900

    feat: ProductController에 상품등록 API 구현
    - 등록 성공 시 201 Created로 응답

--- src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
+++ src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
@@ -1,7 +1,27 @@
 package com.example.jjangushrine.domain.product.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.example.jjangushrine.common.ApiResponse;
+import com.example.jjangushrine.domain.product.dto.request.ProductSaveReq;
+import com.example.jjangushrine.domain.product.dto.response.ProductSaveRes;
+import com.example.jjangushrine.domain.product.service.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController("/api/v1")
+@RequiredArgsConstructor
 public class ProductController {
+	private final ProductService productService;
+
+	@PostMapping("/sellers/products")
+	public ResponseEntity<ApiResponse<ProductSaveRes>> saveProduct(@Valid @RequestBody ProductSaveReq productSaveReq) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(ApiResponse.success("상품이 등록됐습니다.", productService.saveProduct(productSaveReq, 1L))); // 1L 인증/인가 추가 시 수정 필요
+	}
 }
