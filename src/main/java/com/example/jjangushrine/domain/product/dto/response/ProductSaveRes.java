7917ebf7d60a52e3c484404a8817409cab210618
duol9 <lhyy0195@gmail.com>
2025-02-02 02:56:33 +0900

    feat: Entity -> ProductSaveRes 변환 메소드 구현

--- src/main/java/com/example/jjangushrine/domain/product/dto/response/ProductSaveRes.java
+++ src/main/java/com/example/jjangushrine/domain/product/dto/response/ProductSaveRes.java
@@ -1,5 +1,6 @@
 package com.example.jjangushrine.domain.product.dto.response;
 
+import com.example.jjangushrine.domain.product.entity.Product;
 import com.example.jjangushrine.domain.product.enums.Category;
 
 public record ProductSaveRes(
@@ -13,4 +14,16 @@ public record ProductSaveRes(
 	Category category
 ) {
 
+	public static ProductSaveRes fromEntity(Product product) {
+		return new ProductSaveRes(
+			product.getId(),
+			product.getStore().getId(),
+			product.getName(),
+			product.getAmount(),
+			product.getDescription(),
+			product.getImage(),
+			product.getStock(),
+			product.getCategory()
+		);
+	}
 }
