24f93c92767033257cb29311457675f25f35f253
duol9 <lhyy0195@gmail.com>
2025-02-02 02:59:12 +0900

    fix: url 매핑 수정
    - @RestController을 @RequestMapping로 착각
    - 발견 후 수정

--- src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
+++ src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jjangushrine.common.ApiResponse;
@@ -14,7 +15,8 @@ import com.example.jjangushrine.domain.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ProductController {
 	private final ProductService productService;
