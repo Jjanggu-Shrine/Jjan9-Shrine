bd6ff195836d8e407865392f2a36b0ef3c72d051
duol9 <lhyy0195@gmail.com>
2025-02-03 01:18:49 +0900

    feat: 상품등록 API 컨트롤러에 @AuthenticationPrincipal 적용
    - sellerId 추출을 위해 CustomUserDetails 사용

--- src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
+++ src/main/java/com/example/jjangushrine/domain/product/controller/ProductController.java
@@ -2,12 +2,14 @@ package com.example.jjangushrine.domain.product.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jjangushrine.common.ApiResponse;
+import com.example.jjangushrine.config.security.entity.CustomUserDetails;
 import com.example.jjangushrine.domain.product.dto.request.ProductSaveReq;
 import com.example.jjangushrine.domain.product.dto.response.ProductSaveRes;
 import com.example.jjangushrine.domain.product.service.ProductService;
@@ -22,8 +24,11 @@ public class ProductController {
 	private final ProductService productService;
 
 	@PostMapping("/sellers/products")
-	public ResponseEntity<ApiResponse<ProductSaveRes>> saveProduct(@Valid @RequestBody ProductSaveReq productSaveReq) {
+	public ResponseEntity<ApiResponse<ProductSaveRes>> saveProduct(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@Valid @RequestBody ProductSaveReq productSaveReq) {
 		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(ApiResponse.success("상품이 등록됐습니다.", productService.saveProduct(productSaveReq, 1L))); // 1L 인증/인가 추가 시 수정 필요
+			.body(ApiResponse.success("상품이 등록됐습니다.",
+				productService.saveProduct(productSaveReq, userDetails.getId()))); // 1L 인증/인가 추가 시 수정 필요
 	}
 }
