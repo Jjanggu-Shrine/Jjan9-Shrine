a417fd17645cb50af9c99ae21278b3c8e14c37cf
duol9 <lhyy0195@gmail.com>
2025-02-03 09:50:43 +0900

    feat: Product 수정 비즈니스 로직 구현

--- src/main/java/com/example/jjangushrine/domain/product/service/ProductService.java
+++ src/main/java/com/example/jjangushrine/domain/product/service/ProductService.java
@@ -1,8 +1,14 @@
 package com.example.jjangushrine.domain.product.service;
 
+import java.beans.FeatureDescriptor;
+import java.util.Arrays;
+
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.jjangushrine.domain.product.dto.request.ProductUpdateReq;
+import com.example.jjangushrine.domain.product.dto.response.ProductRes;
 import com.example.jjangushrine.domain.product.entity.Product;
 import com.example.jjangushrine.domain.product.exception.ProductAccessDeniedException;
 import com.example.jjangushrine.domain.product.exception.ProductNotFoundException;
@@ -20,6 +26,17 @@ public class ProductService {
 	private final ProductRepository productRepository;
 	private final StoreRepository storeRepository; // 추후 Service를 참조하도록 변경하기
 	private final SellerRepository sellerRepository; // 마찬가지
+
+	@Transactional
+	public ProductRes updateProduct(Long productId, Long sellerId, ProductUpdateReq updateReq) {
+
+		Product findProduct = getProductById(productId);
+		validateProductOwnedBySeller(productId, sellerId);
+
+		findProduct.update(updateReq);
+
+		return ProductRes.fromEntity(findProduct);
+	}
 
 	@Transactional
 	public void deleteProduct(Long sellerId, Long productId) {
