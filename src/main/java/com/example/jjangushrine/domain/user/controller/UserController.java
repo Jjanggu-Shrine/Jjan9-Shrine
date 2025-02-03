cc52cc44c7b467b7acaa95e46b218e97b376a9d0
goo3oo <goo3oo0918@gmail.com>
2025-02-03 22:34:43 +0900

    feat: User 수정 기능 추가

--- src/main/java/com/example/jjangushrine/domain/user/controller/UserController.java
+++ src/main/java/com/example/jjangushrine/domain/user/controller/UserController.java
@@ -3,15 +3,14 @@ package com.example.jjangushrine.domain.user.controller;
 import com.example.jjangushrine.common.ApiResMessage;
 import com.example.jjangushrine.common.ApiResponse;
 import com.example.jjangushrine.config.security.entity.CustomUserDetails;
+import com.example.jjangushrine.domain.user.dto.request.UserUpdateReq;
 import com.example.jjangushrine.domain.user.dto.response.UserRes;
 import com.example.jjangushrine.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -28,5 +27,26 @@ public class UserController {
                 .body(ApiResponse.success(
                         ApiResMessage.GET_USER_SUCCESS,
                         userService.getUserInfo(userDetails.getEmail())));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse<UserRes>> updateUser(
+            @RequestBody UserUpdateReq userUpdateReq,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        ApiResMessage.UPDATE_USER_SUCCESS,
+                        userService.updateUser(userUpdateReq, userDetails.getEmail())));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<Void>> deleteUser(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        userService.deleteUser(userDetails.getEmail());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        ApiResMessage.DELETE_USER_SUCCESS));
     }
 }
--- src/main/java/com/example/jjangushrine/domain/user/service/UserService.java
+++ src/main/java/com/example/jjangushrine/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.jjangushrine.domain.user.service;
 
+import com.example.jjangushrine.domain.user.dto.request.UserUpdateReq;
 import com.example.jjangushrine.domain.user.dto.response.UserRes;
 import com.example.jjangushrine.domain.user.entity.User;
 import com.example.jjangushrine.domain.user.repository.UserRepository;
@@ -7,11 +8,13 @@ import com.example.jjangushrine.exception.ErrorCode;
 import com.example.jjangushrine.exception.common.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
@@ -19,6 +22,19 @@ public class UserService {
     public UserRes getUserInfo(String email) {
         User user = findUserByEmail(email);
         return UserRes.from(user);
+    }
+
+    @Transactional
+    public UserRes updateUser(UserUpdateReq userUpdateReq, String email) {
+        User user = findUserByEmail(email);
+        user.update(userUpdateReq);
+        return UserRes.from(user);
+    }
+
+    @Transactional
+    public void deleteUser(String email) {
+        User user = findUserByEmail(email);
+        user.softDelete();
     }
 
     public boolean existsByEmail(String email) {
