6cc5319c552c4e590de4920fcab80a3f6566bef8
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-03 19:32:52 +0900

    feat: 장바구니 조회

---
+++ src/main/java/com/example/jjangushrine/domain/product/repository/ProductRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.example.jjangushrine.domain.product.repository;
+
+import static com.example.jjangushrine.domain.product.entity.QProduct.*;
+import static com.example.jjangushrine.domain.seller.entity.QSeller.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+public class ProductRepositoryImpl implements ProductRepositoryCustom{
+
+	private final JPAQueryFactory queryFactory;
+
+	public ProductRepositoryImpl(EntityManager entityManager) {
+		this.queryFactory = new JPAQueryFactory(entityManager);
+	}
+
+	@Override
+	public boolean existsByProductIdAndSellerId(Long productId, Long sellerId) {
+		return queryFactory
+			.selectOne()
+			.from(product)
+			.join(product.store.sellerId, seller)
+			.where(
+				product.id.eq(productId),
+				seller.id.eq(sellerId)
+			)
+			.fetchOne() != null;
+	}
+}
