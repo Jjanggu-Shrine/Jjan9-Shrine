9a807394dc9c8c7b98fd340e8ee950326702edeb
duol9 <lhyy0195@gmail.com>
2025-02-03 12:12:08 +0900

    feat: ProductSaveRequset -> Entity 변환 메서드 구현
    - 로직의 가독성을 향상시키기 위해 정적 팩토리 메서드 사용

--- src/main/java/com/example/jjangushrine/domain/product/service/ProductService.java
+++ src/main/java/com/example/jjangushrine/domain/product/service/ProductService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.jjangushrine.domain.product.dto.request.ProductSaveReq;
-import com.example.jjangushrine.domain.product.dto.response.ProductSaveRes;
+import com.example.jjangushrine.domain.product.dto.response.ProductRes;
 import com.example.jjangushrine.domain.product.entity.Product;
 import com.example.jjangushrine.domain.product.exception.StoreAccessDeniedException;
 import com.example.jjangushrine.domain.product.repository.ProductRepository;
@@ -27,7 +27,7 @@ public class ProductService {
 	private final SellerRepository sellerRepository; // 마찬가지
 
 	@Transactional
-	public ProductSaveRes saveProduct(ProductSaveReq productSaveReq, Long sellerId) {
+	public ProductRes saveProduct(ProductSaveReq productSaveReq, Long sellerId) {
 
 		// storeService에 해당 로직 메서드 생기면 변경 예정
 		Store store = storeRepository.findById(productSaveReq.storeId())
@@ -37,19 +37,12 @@ public class ProductService {
 			throw new StoreAccessDeniedException(); // Store에서 관련된 예외처리 생성 시 그걸로 변경하기
 		}
 
-		Product product = Product.builder()
-			.name(productSaveReq.name())
-			.stock(productSaveReq.stock())
-			.store(store)
-			.image(productSaveReq.image())
-			.amount(productSaveReq.amount())
-			.description(productSaveReq.description())
-			.category(productSaveReq.category())
-			.build();
+		Product product = productSaveReq.toEntity();
+		product.registerStore(store);
 
 		productRepository.save(product);
 
-		return ProductSaveRes.fromEntity(product);
+		return ProductRes.fromEntity(product);
 	}
 
 	protected boolean validateStoreAccessForSeller(Store store, Long sellerId) {
