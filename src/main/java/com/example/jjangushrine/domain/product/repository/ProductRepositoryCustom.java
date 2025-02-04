5fcf5046ddb2bbd62804b8783120c591c2bf9678
duol9 <lhyy0195@gmail.com>
2025-02-04 19:12:00 +0900

    feat: Product 카테고리 조회 QueryDSl로 구현

--- src/main/java/com/example/jjangushrine/domain/product/repository/ProductRepositoryCustom.java
+++ src/main/java/com/example/jjangushrine/domain/product/repository/ProductRepositoryCustom.java
@@ -1,6 +1,13 @@
 package com.example.jjangushrine.domain.product.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.jjangushrine.domain.product.dto.response.ProductRes;
+import com.example.jjangushrine.domain.product.enums.Category;
+
 public interface ProductRepositoryCustom {
 
 	boolean existsByProductIdAndSellerId(Long productId, Long sellerId);
+	Page<ProductRes> findAllProductByCategory(Category category, Pageable pageable);
 }
