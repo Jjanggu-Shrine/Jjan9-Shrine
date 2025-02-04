194dc4bf97f3a098bc379764de5f8e0eddadfa9a
goo3oo <goo3oo0918@gmail.com>
2025-02-04 12:06:15 +0900

    feat: 쇼핑몰 조회 기능 추가

--- src/main/java/com/example/jjangushrine/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/jjangushrine/domain/store/repository/StoreRepository.java
@@ -1,8 +1,11 @@
 package com.example.jjangushrine.domain.store.repository;
 
+import com.example.jjangushrine.domain.seller.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.jjangushrine.domain.store.entity.Store;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findBySellerIdAndIsDeletedFalse(Seller seller);
 }
