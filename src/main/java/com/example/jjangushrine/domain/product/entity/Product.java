37e3d18b278717161a3a1e814e402e766109cbec
duol9 <lhyy0195@gmail.com>
2025-02-05 15:42:43 +0900

    fix #69 : 성능 개선을 위해 Index column 설정
    - 100만건의 데이터를 필터링 하여 조회 시 평균적으로 약 5.6초가 걸리는 것을 확인
    - 성능 개선이 필요하다 판단해 인덱스 사용
    - 적용 후 평균 1.5초로 줄어듦

--- src/main/java/com/example/jjangushrine/domain/product/entity/Product.java
+++ src/main/java/com/example/jjangushrine/domain/product/entity/Product.java
@@ -19,6 +19,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -29,7 +30,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-@Table(name = "products")
+@Table(name = "products", indexes = @Index(name = "idx_group", columnList = "category, store_id, is_deleted"))
 @EntityListeners(AuditingEntityListener.class)
 public class Product extends BaseEntity {
 	@Id
