ffac66f07c001eb60ceae4c447cc30e5228de031
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-06 09:32:30 +0900

    Refactor : Valid 상수 메세지 처리

--- src/main/java/com/example/jjangushrine/domain/cart/dto/request/CartItemCreateReq.java
+++ src/main/java/com/example/jjangushrine/domain/cart/dto/request/CartItemCreateReq.java
@@ -1,14 +1,15 @@
 package com.example.jjangushrine.domain.cart.dto.request;
 
 
+import com.example.jjangushrine.domain.cart.dto.CartValidationMessage;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 
 public record CartItemCreateReq(
-        @NotBlank(message = "productId를 입력해주세요.")
+        @NotBlank(message = CartValidationMessage.PRODUCT_BLANK_MESSAGE)
         @JsonProperty("productId") Long productId,
-        @NotBlank(message = "수량을 입력해주세요.")
+        @NotBlank(message = CartValidationMessage.QUANTITY_BLANK_MESSAGE)
         @JsonProperty("quantity") Short quantity
 ) {
 }
