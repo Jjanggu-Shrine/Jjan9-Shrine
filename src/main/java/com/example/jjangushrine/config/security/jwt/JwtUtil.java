9b5d89cf17866d75516175d0bb33848bb11a1c5c
goo3oo <goo3oo0918@gmail.com>
2025-02-07 03:16:18 +0900

    refactor: API 인가 권한 추가

--- src/main/java/com/example/jjangushrine/config/security/jwt/JwtUtil.java
+++ src/main/java/com/example/jjangushrine/config/security/jwt/JwtUtil.java
@@ -1,49 +1,49 @@
-    package com.example.jjangushrine.config.security.jwt;
+package com.example.jjangushrine.config.security.jwt;
 
-    import com.example.jjangushrine.config.security.SecurityConst;
-    import com.example.jjangushrine.config.security.entity.CustomUserDetails;
-    import io.jsonwebtoken.*;
-    import io.jsonwebtoken.security.Keys;
-    import lombok.extern.slf4j.Slf4j;
-    import org.springframework.beans.factory.annotation.Value;
-    import org.springframework.security.core.Authentication;
-    import org.springframework.stereotype.Component;
+import com.example.jjangushrine.config.security.SecurityConst;
+import com.example.jjangushrine.config.security.entity.CustomUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
-    import javax.crypto.SecretKey;
-    import java.util.Base64;
-    import java.util.Date;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
 
-    @Slf4j
-    @Component
-    public class JwtUtil {
+@Slf4j
+@Component
+public class JwtUtil {
 
-        private final SecretKey secretKey;
+    private final SecretKey secretKey;
 
-        public JwtUtil(@Value("${jwt.secret.key}") String secret) {
-            byte[] keyBytes = Base64.getDecoder().decode(secret);
-            this.secretKey = Keys.hmacShaKeyFor(keyBytes);
-        }
-
-        public String generateToken(Authentication authentication) {
-            CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
-            Date now = new Date();
-
-            return SecurityConst.BEARER_PREFIX + Jwts.builder()
-                    .subject(principal.getId().toString())
-                    .claim("email", principal.getUsername())
-                    .claim("role", principal.getRole().name())
-                    .issuedAt(now)
-                    .expiration(new Date(now.getTime() + SecurityConst.TOKEN_TIME))
-                    .signWith(secretKey)
-                    .compact();
-        }
-
-        public Claims extractClaims(String token) {
-            return Jwts.parser()
-                    .verifyWith(secretKey)
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload();
-        }
+    public JwtUtil(@Value("${jwt.secret.key}") String secret) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String generateToken(Authentication authentication) {
+        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+        Date now = new Date();
+
+        return SecurityConst.BEARER_PREFIX + Jwts.builder()
+                .subject(principal.getId().toString())
+                .claim("email", principal.getUsername())
+                .claim("role", principal.getRole().name())
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + SecurityConst.TOKEN_TIME))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
 
