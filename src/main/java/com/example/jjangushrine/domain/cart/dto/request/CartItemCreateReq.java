e82d3453d4485b4f24359a21e7b123a572e75251
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-06 09:17:09 +0900

    fix: 타입 변경 수정

--- src/main/java/com/example/jjangushrine/domain/cart/dto/request/CartItemCreateReq.java
+++ src/main/java/com/example/jjangushrine/domain/cart/dto/request/CartItemCreateReq.java
@@ -2,10 +2,13 @@ package com.example.jjangushrine.domain.cart.dto.request;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 
 public record CartItemCreateReq(
+        @NotBlank(message = "productId를 입력해주세요.")
         @JsonProperty("productId") Long productId,
+        @NotBlank(message = "수량을 입력해주세요.")
         @JsonProperty("quantity") Short quantity
 ) {
 }
